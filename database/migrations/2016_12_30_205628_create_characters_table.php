cd8f2954edf2c5fddb938d8611f5c76f4d52c45e
vagrant <vagrant@homestead>
2016-12-30 21:12:23 +0000

    Créations des fichiers migration

---
+++ database/migrations/2016_12_30_205628_create_characters_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCharactersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('pseudo');
+            $table->string('race');
+            $table->string('classe');
+            $table->integer('level');
+            $table->string('faction');
+            $table->integer('itemLevel');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
---
+++ database/migrations/2016_12_30_205704_create_character_events_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCharacterEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('character_events', function (Blueprint $table) {
+            $table->integer('event_id')->unsigned();
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->integer('characters_id')->unsigned();
+            $table->foreign('characters_id')->references('id')->on('characters');
+            $table->boolean('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('character_events');
+    }
+}
